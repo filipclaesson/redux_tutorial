6e1e100fbf6071cc4dcd58d07bc6112e5eb1c034
Filip Claesson <Filip@Filips-MacBook-Air.local>
2018-01-29 20:26:03 +0100

    add in bash_commands.sh

--- bash_commands.sh
+++ bash_commands.sh
@@ -3,3 +3,14 @@ npm init -y
 npm i --save-dev webpack
 npm i --save express
 npm i --save babel-core babel-loader babel-preset-es2015 babel-preset-stage-1 babel-preset-react
+
+
+
+# setup sequelize
+#https://scotch.io/tutorials/getting-started-with-node-express-and-postgres-using-sequelize
+npm install --save sequelize pg pg-hstore
+sequelize init
+
+
+
+createdb udemy-tutorial
